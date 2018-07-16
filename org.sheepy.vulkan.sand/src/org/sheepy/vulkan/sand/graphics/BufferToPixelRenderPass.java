88a590925b82c52f5dde87b9c2e7fff6059161ed
Earu <ealrann@gmail.com>
2018-07-16 11:17:29 +0800

    [sand] Update UI

--- org.sheepy.vulkan.sand/src/org/sheepy/vulkan/sand/graphics/BufferToPixelRenderPass.java
+++ org.sheepy.vulkan.sand/src/org/sheepy/vulkan/sand/graphics/BufferToPixelRenderPass.java
@@ -30,6 +30,7 @@ public class BufferToPixelRenderPass implements IRenderPass
 	private long renderPass;
 
 	private ImGuiPipeline myImGui;
+	private VkImageBlit.Buffer region;
 
 	public BufferToPixelRenderPass(BufferedSwapConfiguration configuration)
 	{
@@ -38,6 +39,25 @@ public class BufferToPixelRenderPass implements IRenderPass
 		this.configuration = configuration;
 
 		this.myImGui = configuration.imGui;
+		
+		region = VkImageBlit.calloc(1);
+		region.srcSubresource().aspectMask(VK_IMAGE_ASPECT_COLOR_BIT);
+		region.srcSubresource().mipLevel(0);
+		region.srcSubresource().baseArrayLayer(0);
+		region.srcSubresource().layerCount(1);
+		region.srcOffsets(0).x(0);
+		region.srcOffsets(0).y(0);
+		region.srcOffsets(0).z(0);
+		region.srcOffsets(1).x(srcImage.getWidth());
+		region.srcOffsets(1).y(srcImage.getHeight());
+		region.srcOffsets(1).z(1);
+		region.dstSubresource().aspectMask(VK_IMAGE_ASPECT_COLOR_BIT);
+		region.dstSubresource().mipLevel(0);
+		region.dstSubresource().baseArrayLayer(0);
+		region.dstSubresource().layerCount(1);
+		region.dstOffsets(0).x(0);
+		region.dstOffsets(0).y(0);
+		region.dstOffsets(0).z(0);
 	}
 
 	@Override
@@ -83,24 +103,6 @@ public class BufferToPixelRenderPass implements IRenderPass
 		long bltSrcImage = srcImage.getId();
 		long bltDstImage = dstImageView.getImageId();
 
-		VkImageBlit.Buffer region = VkImageBlit.calloc(1);
-		region.srcSubresource().aspectMask(VK_IMAGE_ASPECT_COLOR_BIT);
-		region.srcSubresource().mipLevel(0);
-		region.srcSubresource().baseArrayLayer(0);
-		region.srcSubresource().layerCount(1);
-		region.srcOffsets(0).x(0);
-		region.srcOffsets(0).y(0);
-		region.srcOffsets(0).z(0);
-		region.srcOffsets(1).x(srcImage.getWidth());
-		region.srcOffsets(1).y(srcImage.getHeight());
-		region.srcOffsets(1).z(1);
-		region.dstSubresource().aspectMask(VK_IMAGE_ASPECT_COLOR_BIT);
-		region.dstSubresource().mipLevel(0);
-		region.dstSubresource().baseArrayLayer(0);
-		region.dstSubresource().layerCount(1);
-		region.dstOffsets(0).x(0);
-		region.dstOffsets(0).y(0);
-		region.dstOffsets(0).z(0);
 		region.dstOffsets(1).x(extent.getWidth());
 		region.dstOffsets(1).y(extent.getHeight());
 		region.dstOffsets(1).z(1);
@@ -116,14 +118,13 @@ public class BufferToPixelRenderPass implements IRenderPass
 
 		dstImageView.transitionImageLayout(commandBuffer.getVkCommandBuffer(),
 				VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, 1,
-				VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
-				VK_ACCESS_TRANSFER_WRITE_BIT, VK_ACCESS_COLOR_ATTACHMENT_READ_BIT);
+				VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_VERTEX_SHADER_BIT,
+				VK_ACCESS_TRANSFER_WRITE_BIT, VK_ACCESS_SHADER_WRITE_BIT);
 	}
 
 	@Override
 	public void allocate(MemoryStack stack)
 	{
-
 		VkAttachmentDescription colorAttachment = VkAttachmentDescription.calloc();
 		colorAttachment.format(configuration.swapChainManager.getColorDomain().getColorFormat());
 		colorAttachment.samples(VK_SAMPLE_COUNT_1_BIT);
@@ -146,7 +147,7 @@ public class BufferToPixelRenderPass implements IRenderPass
 		VkSubpassDependency.Buffer dependency = VkSubpassDependency.calloc(1);
 		dependency.srcSubpass(VK_SUBPASS_EXTERNAL);
 		dependency.dstSubpass(0);
-		dependency.srcStageMask(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT);
+		dependency.srcStageMask(VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT);
 		dependency.srcAccessMask(0);
 		dependency.dstStageMask(VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT);
 		dependency.dstAccessMask(
@@ -191,6 +192,7 @@ public class BufferToPixelRenderPass implements IRenderPass
 	@Override
 	public void free()
 	{
+		region.free();
 		myImGui.free();
 		vkDestroyRenderPass(logicalDevice.getVkDevice(), renderPass, null);
 	}
