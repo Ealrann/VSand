4b0c2faf88cb143503ab5b7a1640870a95aa54ba
Aurélien Mora <ealrann@gmail.com>
2019-06-09 09:23:25 +0200

    Redraw benchmark

--- org.sheepy.vsand/src/main/java/org/sheepy/vsand/VSandBenchmarkLauncher.java
+++ org.sheepy.vsand/src/main/java/org/sheepy/vsand/VSandBenchmarkLauncher.java
@@ -15,7 +15,7 @@ public class VSandBenchmarkLauncher
 	public static void main(String[] args)
 	{
 		final var application = createTestApplication();
-		final var mainLoop = VSandMainLoop.createBenchmark(application, 2500);
+		final var mainLoop = VSandMainLoop.createBenchmark(application, 3000);
 
 		ApplicationLauncher.launch(application, mainLoop);
 	}
@@ -28,8 +28,7 @@ public class VSandBenchmarkLauncher
 		EcoreUtil.delete(application.getViews().get(0));
 
 		final DemoDrawer drawer = new DemoDrawer(application);
-		drawer.drawLavaPot(300, 400);
-		drawer.drawLavaPot(650, 200);
+		drawer.drawLavaPot(200, 400);
 
 		return application;
 	}
@@ -40,45 +39,54 @@ public class VSandBenchmarkLauncher
 		private final EList<DrawCommand> drawQueue;
 		private final EList<Material> materialList;
 		private final Material sand;
+		private final Material plant;
 		private final Material wall;
 		private final Material water;
-		private final Material wax;
 		private final Material lava;
-		private final Material fuel;
+		private final Material petrol;
 
 		public DemoDrawer(VSandApplication application)
 		{
 			drawQueue = application.getDrawQueue();
 			materialList = application.getMaterials().getMaterials();
 			sand = findMaterial(materialList, "Sand");
+			plant = findMaterial(materialList, "Plant");
 			wall = findMaterial(materialList, "Wall");
 			water = findMaterial(materialList, "Water");
-			wax = findMaterial(materialList, "Wax");
 			lava = findMaterial(materialList, "Lava");
-			fuel = findMaterial(materialList, "Fuel");
+			petrol = findMaterial(materialList, "Petrol");
 		}
 
 		public void drawLavaPot(int x, int y)
 		{
-			drawQueue.add(drawCircle(wall, x, y, 24));
-			drawQueue.add(drawLine(wall, x, y, x, y + 200, 24));
-			drawQueue.add(drawCircle(wall, x, y + 200, 24));
-			drawQueue.add(drawLine(wall, x, y + 200, x + 300, y + 200, 24));
-			drawQueue.add(drawCircle(wall, x + 300, y + 200, 24));
-			drawQueue.add(drawLine(wall, x + 300, y + 200, x + 300, y, 24));
-			drawQueue.add(drawCircle(wall, x + 300, y, 24));
+			drawQueue.add(drawCircle(wall, x, y, 20));
+			drawQueue.add(drawLine(wall, x, y, x + 150, y + 150, 20));
+			drawQueue.add(drawLine(wall, x + 150, y + 150, x + 750, y + 150, 20));
+			drawQueue.add(drawLine(wall, x + 750, y + 150, x + 900, y, 20));
 
-			drawQueue.add(drawLine(sand, x + 20, y + 150, x + 270, y + 150, 32));
-			drawQueue.add(drawLine(water, x + 20, y + 110, x + 270, y + 110, 32));
-			drawQueue.add(drawLine(fuel, x + 20, y + 70, x + 270, y + 70, 32));
+			drawQueue.add(drawCircle(sand, x + 100, y + 50, 32));
+			drawQueue.add(drawLine(sand, x + 100, y + 50, x + 800, y + 50, 32));
 
-			drawQueue.add(drawCircle(wax, x + 25, y, 24));
-			drawQueue.add(drawLine(wax, x + 25, y, x + 275, y, 24));
+			drawQueue.add(drawCircle(water, x + 100, y + 20, 32));
+			drawQueue.add(drawLine(water, x + 100, y + 20, x + 800, y + 20, 32));
 
-			drawQueue.add(drawCircle(lava, x + 150, y - 100, 96));
+			drawQueue.add(drawCircle(sand, x + 300, y + 5, 64));
+			drawQueue.add(drawCircle(sand, x + 350, y - 5, 64));
+			drawQueue.add(drawCircle(sand, x + 400, y + 5, 64));
+			drawQueue.add(drawCircle(sand, x + 600, y + 20, 64));
+
+			drawQueue.add(drawCircle(sand, x + 600, y + 20, 64));
+
+			drawQueue.add(drawCircle(plant, x + 770, y + 80, 2));
+
+			drawQueue.add(drawLine(petrol, x + 100, y - 10, x + 280, y - 10, 16));
+
+			drawQueue.add(drawCircle(lava, x + 150, y - 220, 96));
+			drawQueue.add(drawCircle(lava, x + 450, y - 350, 96));
+			drawQueue.add(drawCircle(lava, x + 750, y - 250, 96));
 		}
 
-		private DrawCommand drawCircle(final Material sand, int x, int y, int size)
+		private static DrawCommand drawCircle(final Material sand, int x, int y, int size)
 		{
 			final var draw = VSandFactory.eINSTANCE.createDrawCircle();
 			draw.setMaterial(sand);
@@ -88,7 +96,12 @@ public class VSandBenchmarkLauncher
 			return draw;
 		}
 
-		private DrawCommand drawLine(final Material sand, int x1, int y1, int x2, int y2, int size)
+		private static DrawCommand drawLine(final Material sand,
+											int x1,
+											int y1,
+											int x2,
+											int y2,
+											int size)
 		{
 			final var draw = VSandFactory.eINSTANCE.createDrawLine();
 			draw.setMaterial(sand);
