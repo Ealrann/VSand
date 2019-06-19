d0745f2891054de30ea2f1ad39f097e8fa57582f
Aurélien Mora <ealrann@gmail.com>
2019-06-19 21:26:12 +0200

    Automatically copy run scripts during jlink task (windows)

---
+++ org.sheepy.vsand/releng/windows/benchmark/VSand_Benchmark_Headless.bat
@@ -0,0 +1,5 @@
+@echo off
+set DIR="%~dp0\..\bin"
+set JAVA_EXEC="%DIR:"=%\java"
+pushd %DIR% & %JAVA_EXEC% -Dlog4j.configurationFile=./log4j2.xml -m org.sheepy.vsand/org.sheepy.vsand.VSandHeadlessBenchmarkLauncher  %* & popd
+pause
