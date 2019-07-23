66ec9ad84e8b22e2c3fb61e8fc1cf0ae28308c56
Aurélien Mora <ealrann@gmail.com>
2019-07-23 14:31:40 +0800

    Change -Xshareclasses directory

--- org.sheepy.vsand/releng/windows/VSand.bat
+++ org.sheepy.vsand/releng/windows/VSand.bat
@@ -1,4 +1,4 @@
 @echo off
 set DIR="%~dp0\bin"
 set JAVA_EXEC="%DIR:"=%\java"
-pushd %DIR% & %JAVA_EXEC% -p ..\modules -Xshareclasses -m org.sheepy.vsand/org.sheepy.vsand.VSandApplicationLauncher  %* & popd
+pushd %DIR% & %JAVA_EXEC% -p ..\modules -Xshareclasses:cacheDir=sharedClasses -m org.sheepy.vsand/org.sheepy.vsand.VSandApplicationLauncher  %* & popd
