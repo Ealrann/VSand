5704188e8714847d4f1233a27e0191b4cb1e9481
Aurélien Mora <ealrann@gmail.com>
2019-08-21 23:54:35 +0800

    BoardImageLoader is no longer a statefull adapter

--- org.sheepy.vsand/src/main/java/org/sheepy/vsand/loader/BoardImageLoader.java
+++ org.sheepy.vsand/src/main/java/org/sheepy/vsand/loader/BoardImageLoader.java
@@ -4,13 +4,11 @@ import static org.lwjgl.vulkan.VK10.*;
 
 import org.sheepy.lily.core.api.adapter.annotation.Adapter;
 import org.sheepy.lily.core.api.adapter.annotation.Autorun;
-import org.sheepy.lily.core.api.adapter.annotation.Statefull;
 import org.sheepy.lily.core.api.util.ModelUtil;
 import org.sheepy.lily.vulkan.api.adapter.IVulkanAdapter;
 import org.sheepy.lily.vulkan.model.resource.StaticImage;
 import org.sheepy.vsand.model.VSandApplication;
 
-@Statefull
 @Adapter(scope = StaticImage.class, name = "Board Image")
 public final class BoardImageLoader implements IVulkanAdapter
 {
