ae9655b3c2f324692b938dd1fe259556f35d713e
Aurélien Mora <ealrann@gmail.com>
2020-01-08 14:26:56 +0100

    Limit openj9 cache size

--- org.sheepy.vsand/releng/windows/VSand.bat
+++ org.sheepy.vsand/releng/windows/VSand.bat
@@ -1,4 +1,4 @@
 @echo off
 set DIR="%~dp0\bin"
 set JAVA_EXEC="%DIR:"=%\java"
-pushd %DIR% & %JAVA_EXEC% -Xms32M -Xmx64M -p ..\modules -Xshareclasses:cacheDir=sharedClasses -m org.sheepy.vsand/org.sheepy.vsand.VSandApplicationLauncher  %* & popd
+pushd %DIR% & %JAVA_EXEC% -Xms32M -Xmx64M -p ..\modules -Xscmx32M -Xshareclasses:cacheDir=sharedClasses,name=VSand,nonfatal -m org.sheepy.vsand/org.sheepy.vsand.VSandApplicationLauncher  %* & popd
