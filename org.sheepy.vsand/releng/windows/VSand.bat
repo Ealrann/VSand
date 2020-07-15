c35a2bde3c565b7d73afcb91740674ee8763b9a7
Aurélien Mora <ealrann@gmail.com>
2020-07-15 21:03:32 +0200

    Fix launch bat on windows

--- org.sheepy.vsand/releng/windows/VSand.bat
+++ org.sheepy.vsand/releng/windows/VSand.bat
@@ -1,5 +1,5 @@
 @echo off
 set DIR="%~dp0\bin"
 set JAVA_EXEC="%DIR:"=%\java"
-set ARGS="-Xms32M -Xmx64M --enable-preview"
+set ARGS=-Xms32M -Xmx64M --enable-preview
 pushd %DIR% & %JAVA_EXEC% %ARGS% -p ..\modules -m org.sheepy.vsand/org.sheepy.vsand.VSandApplicationLauncher  %* & popd
--- org.sheepy.vsand/releng/windows/benchmark/VSand_Benchmark.bat
+++ org.sheepy.vsand/releng/windows/benchmark/VSand_Benchmark.bat
@@ -1,6 +1,6 @@
 @echo off
 set DIR="%~dp0\..\bin"
 set JAVA_EXEC="%DIR:"=%\java"
-set ARGS="-Xms64M -Xmx128M --enable-preview"
+set ARGS=-Xms64M -Xmx128M --enable-preview
 pushd %DIR% & %JAVA_EXEC% %ARGS% -p ..\modules -m org.sheepy.vsand/org.sheepy.vsand.VSandBenchmarkLauncher  %* & popd
 pause
